2a1f8ccc65b4b6cc5f833550ef66f5c8226d7056
neetmangat <neetmangat@gmail.com>
2021-02-06 05:41:42 -0600

    Initial Commit from last project

---
+++ src/Row.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import "./Row.css";
+import YouTube from "react-youtube";
+import requests from "./requests";
+
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+function Row({ title, fetchUrl, isLargeRow }) {
+  const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
+
+  // A snippet of code which runs based on a specific condition/variable
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = async (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      const trailerUrl = await axios.get(
+        `/movie/${movie.id}${requests.fetchMovieTrailer}`
+      );
+      setTrailerUrl(trailerUrl.data.results[0]?.key);
+    }
+  };
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+
+      <div className="row_posters">
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            onClick={() => handleClick(movie)}
+            className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.title}
+          />
+        ))}
+      </div>
+
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+    </div>
+  );
+}
+
+export default Row;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBAusOQdeJlsqaTTT_UXScgbug-KtQRJdM",
+  authDomain: "flix-97e6f.firebaseapp.com",
+  projectId: "flix-97e6f",
+  storageBucket: "flix-97e6f.appspot.com",
+  messagingSenderId: "235525540431",
+  appId: "1:235525540431:web:f717fbabfa3522c57d783a",
+  measurementId: "G-MH40N2VX1Y",
+};
