2ccf6f8d3e6e35690c80a459dbfa034a818443bf
neetmangat <neetmangat@gmail.com>
2021-02-09 00:03:47 -0600

    finished authentication with sign out, redux, and edit profile page

--- src/App.js
+++ src/App.js
@@ -3,21 +3,31 @@ import HomeScreen from "./screens/HomeScreen";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import LoginScreen from "./screens/LoginScreen";
+import ProfileScreen from "./screens/ProfileScreen";
 import { auth } from "./firebase";
+import { useDispatch, useSelector } from "react-redux";
+import { logout, login, selectUser } from "./features/userSlice";
 
 function App() {
-  const user = null;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    auth.onAuthStateChanged((userAuth) => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
       if (userAuth) {
-        // Logged in
-        console.log(userAuth);
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
       } else {
-        // Logged out
+        dispatch(logout());
       }
     });
-  }, []);
+
+    return unsubscribe;
+  }, [dispatch]);
 
   return (
     <div className="app">
@@ -26,6 +36,9 @@ function App() {
           <LoginScreen />
         ) : (
           <Switch>
+            <Route path="/profile">
+              <ProfileScreen />
+            </Route>
             <Route exact path="/">
               <HomeScreen />
             </Route>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,26 +1,36 @@
 import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
 import logo from "../assets/images/flixer_logo.png";
 import "./Nav.css";
 
 function Nav() {
   const [handleShow, setHandleShow] = useState(false);
+  const history = useHistory();
+
+  const transitionNavBar = () => {
+    if (window.scrollY > 100) {
+      setHandleShow(true);
+    } else {
+      setHandleShow(false);
+    }
+  };
 
   useEffect(() => {
-    window.addEventListener("scroll", () => {
-      if (window.scrollY > 100) {
-        setHandleShow(true);
-      } else setHandleShow(false);
-    });
-    return () => {
-      window.removeEventListener("scroll");
-    };
+    window.addEventListener("scroll", transitionNavBar);
+    return () => window.removeEventListener("scroll", transitionNavBar);
   }, []);
 
   return (
     <div className={`nav ${handleShow && "nav__black"}`}>
       <div className="nav__contents">
-        <img className="nav__logo" src={logo} alt="Flixer logo" />
         <img
+          onClick={() => history.push("/")}
+          className="nav__logo"
+          src={logo}
+          alt="flixer logo"
+        />
+        <img
+          onClick={() => history.push("/profile")}
           className="nav__avatar"
           src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png"
           alt="Avatar"
